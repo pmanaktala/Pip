35fc8ab1b55c797bbb681a05a45b837c0442b2aa
Parth Manaktala <manaktala.parth@gmail.com>
2026-09-12 12:16:42 -0400

    Initial Commit

---
+++ Pip/PipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PipApp.swift
+//  Pip
+//
+//  Created by Parth Manaktala on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
